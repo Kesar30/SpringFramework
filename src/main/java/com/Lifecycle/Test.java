beb318dada546b89dc68b19a42345b48a4818b35
Kesar30 <Kesar4465@gmail.com>
2026-07-08 17:01:20 +0530

    annotation changes added

--- src/main/java/com/Lifecycle/Test.java
+++ src/main/java/com/Lifecycle/Test.java
@@ -1,4 +1,22 @@
 package com.Lifecycle;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class Test {
+    public static void main(String[] args) {
+        //to call destroy method using abstract
+        AbstractApplicationContext context = new ClassPathXmlApplicationContext("LifecycleConfig.xml");
+        simple s = (simple) context.getBean("s");
+        System.out.println(s);
+        context.registerShutdownHook();//it will call destory method
+        //interfacesLC
+        interfacesLC lc = (interfacesLC) context.getBean("lc");
+        System.out.println(lc);
+        //annotation
+        annotationLC a = (annotationLC) context.getBean("a");
+        System.out.println(a);
+    }
+
 }
---
+++ src/main/java/reference/com/B.java
@@ -0,0 +1,20 @@
+package reference.com;
+
+public class B {
+    private int y;
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public B(int y) {
+        this.y = y;
+    }
+
+    public B() {
+    }
+}
